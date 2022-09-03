9c6722f6c73b29c40ff605138f82049fbcc5c7f8
yaoxilong <1299583049@qq.com>
2022-09-03 21:31:00 +0800

    首次提交文件

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<!--
+ * @Author: yaoxilong 1299583049@qq.com
+ * @Date: 2022-09-03 20:46:19
+ * @LastEditors: yaoxilong 1299583049@qq.com
+ * @LastEditTime: 2022-09-03 21:11:03
+ * @FilePath: \cli-init\src\components\HelloWorld.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+<div id="example-1">
+  <button @click="show = !show">
+    Toggle render
+  </button>
+  <transition name="slide-fade">
+    <p v-if="show">hello</p>
+  </transition>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      show: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+/* 可以设置不同的进入和离开动画 */
+/* 设置持续时间和动画函数 */
+.slide-fade-enter-active {
+  transition: all 0.8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter
+/* .slide-fade-leave-active for below version 2.1.8 */ {
+  transform: translateX(-30px);
+  opacity: 0;
+}
+.slide-fade-leave-to
+/* .slide-fade-leave-active for below version 2.1.8 */ {
+  transform: translateY(-30px);
+  opacity: 0;
+}
+</style>
